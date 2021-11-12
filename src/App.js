bc4c20b236effc0b14009610222a719dd8c81d92
Alex <Bavalex@outlook.com>
2021-11-12 11:34:42 +0300

    Изменения главной страницы
    - Новые иконки на главной
    - эффект увеличения
    - новый логотип

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import CardButton from "./components/CardButton/CardButton";
+import logo from './img/logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-            <h1>DICOM.team</h1>
+            <img src={logo} alt="DICOM компания"/>
         </div>
         <div className={'CardsButtons'}>
-            <CardButton number={'1'}/>
-            <CardButton number={'2'}/>
-            <CardButton number={'3'}/>
-            <CardButton number={'4'}/>
-            <CardButton number={'5'}/>
-            <CardButton number={'6'}/>
-            <CardButton number={'7'}/>
-            <CardButton number={'8'}/>
-            <CardButton number={'9'}/>
+            <CardButton number={'1'} name={'Услуги'}/>
+            <CardButton number={'2'} name={'Сервисы'}/>
+            <CardButton number={'3'} name={'Кейсы'}/>
+            <CardButton number={'4'} name={'Команда'}/>
+            <CardButton number={'5'} name={'Вакансии'}/>
+            <CardButton number={'6'} name={'Контакты'} img={'/img/contacts.png'} />
+            <CardButton number={'7'} name={'Продвижение'}/>
+            <CardButton number={'8'} name={'Битрикс24'} img={'/img/bitrix24.png'}/>
+            <CardButton number={'9'} name={'1С'}/>
         </div>
           <div className={'CopyRight'}>
               политика конфиденциальности <br/>
