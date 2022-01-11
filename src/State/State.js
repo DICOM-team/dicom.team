349c4dd2216e2073a94e4945502b90966fd4c85f
vadimklochkov <vadimklochkov@inbox.ru>
2022-01-11 16:41:25 +0300

    Изменение дизайна
    Светлая версия сайта по фигме

--- src/State/State.js
+++ src/State/State.js
@@ -1,13 +1,13 @@
 export let mainMenu = {
     items: [
-        {name: 'Услуги', urm: '/uslugi', img: '/img/icon1.png'},
+        {name: 'Услуги', urm: '/uslugi', img: '/img/dicomYslugi.png'},
         // {name: 'Сервисы', urm: '/services', img: '/img/icon1.png'},
-        {name: 'Кейсы', urm: '/cases', img: '/img/icon2.png'},
-        {name: 'Команда', urm: '/team', img: '/img/icon3.png'},
-        {name: 'Контакты', urm: '/contacts', img: '/img/icon3.png'},
-        {name: 'Support', urm: '/promotion', img: '/img/icon1.png'},
-        {name: 'Битрикс24', urm: '/bitrix24', img: '/img/bitrix24.png'},
-        {name: '1C', urm: '/1c', img: '/img/1с.png'},
-        {name: 'Вакансии', urm: '/vacancys', img: '/img/icon3.png'}
+        {name: 'Кейсы', urm: '/cases', img: '/img/dicomCase.png'},
+        {name: 'Команда', urm: '/team', img: '/img/dicomCommand.png'},
+        {name: 'Контакты', urm: '/contacts', img: '/img/dicomContacts.png'},
+        {name: 'Support', urm: '/promotion', img: '/img/dicomHelp.png'},
+        {name: 'Битрикс24', urm: '/bitrix24', img: '/img/dicomB24.png'},
+        {name: '1C', urm: '/1c', img: '/img/dicom1c.png'},
+        {name: 'Вакансии', urm: '/vacancys', img: '/img/dicomVakan.png'}
     ]
 }
