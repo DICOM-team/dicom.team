a9c2d0e4496e4180c607196b0a362afc2268df6c
vadimklochkov <vadimklochkov@inbox.ru>
2022-01-14 15:17:53 +0300

    изменение стилей
    - Изменение стилей расположения пунктов меню
    - Фикс дёрганности пунктов меню при наведении
    - Добавление bootstrap

--- src/components/CardButton/CardButton.js
+++ src/components/CardButton/CardButton.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import s from './CardButton.module.css'
+import './../../css/flex.min.css';
 
 const CardButton = (props) => {
     let bgr= props.img
     return (
-        <div>
+        <div className="flex-center p-3">
+            <div className={s.cardHover}>
             <div className={s.body} style={{backgroundImage: `url(${process.env.PUBLIC_URL + bgr})`,
                                             backgroundSize: 'contain'}}>
                 {/*{props.number}*/}
             </div>
             <div className={s.name}>
                 {props.name}
+            </div>
             </div>
         </div>
     );
--- src/components/Pages/Main/Main.js
+++ src/components/Pages/Main/Main.js
@@ -7,13 +7,13 @@ const Main = () => {
 
     const Cardbuttons = mainMenu.items.map( (item) => {
         return (
-            <Link to={item.urm}><CardButton name={item.name} img={item.img}/></Link>
+            <div className="col-6 col-md-4 col-xl-3"><Link to={item.urm}><CardButton name={item.name} img={item.img}/></Link></div>
         )
     })
 
     return (
         <div className="App">
-            <div className={'CardsButtons'}>
+            <div className={'CardsButtons row'}>
                 {Cardbuttons}
             </div>
         </div>
