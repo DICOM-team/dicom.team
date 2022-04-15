77c2be5f1df315fdc271ec426d4bbe580517bee0
Alex <Bavalex@outlook.com>
2022-04-15 16:26:53 +0300

    мелкие правки

--- src/components/Pages/Contacts/Contacts.js
+++ src/components/Pages/Contacts/Contacts.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import s from "../Main/Main.module.css";
-import logo from "../../../img/logo.svg";
-import {Link} from "react-router-dom";
+import Title_h1 from "../../Blocks/Title_H1/Title_h1";
+import Title_h2 from "../../Blocks/Title_H2/Title_h2";
+import Accent from "../../Blocks/accent/Accent";
+import Separator from "../../Blocks/Separator/Separator";
 
 const Contacts = () => {
     return (
         <div className="App">
-            Наш телефон: +7 960 311 21 05
+            <Title_h1 name={'Наши контакты'} subname={'самый удобный способ это telegram или WhatsApp'} />
+            <Separator />
+            <b className={'Lage_text'}>
+                Наш телефон: +7 960 311 21 05 <br/>
+                WhatsApp и Telegram <br/>
+                e-mail: bavalex24@yandex.ru
+            </b>
+            <Separator />
+            <Title_h2 name={'Наш офис'} subname={'Чебоксары, пр. Ленина 12Б'} />
+            <Accent context={'Коворкинг центр имеет пропускную систему, поэтому предварительно позвоните мне или любым другим способом сообщите о предстоящей встрече.'}/>
         </div>
     );
 };
