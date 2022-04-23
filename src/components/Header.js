56453b7db56c22d88eb7afa16fc2b78f7fc15b12
Alex <Bavalex@outlook.com>
2022-04-23 23:03:58 +0300

    иконка домой

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,19 @@ import logo from "../img/logo.svg";
 
 const Header = () => {
     return (
-        <div>
-            <div className={s.logo}>
-                <Link to={'/'}><img src={logo} alt="DICOM компания"/></Link>
-                {/*<p>*/}
-                {/*    Нажмите на логотип, чтобы вернуться на домашнюю станицу*/}
-                {/*</p>*/}
+        <>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className={'col-9 col-xs-8 col-md-6'}>
+                    <div className={s.logo}>
+                        <Link to={'/'}><img src={logo} alt="DICOM компания"/></Link>
+                    </div>
+                </div>
+                <div className={'col-3 col-xs-2 col-md-3 flex-center'}>
+                    <Link to={'/'}><img src="/img/home.png" width={'30px'}/></Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
