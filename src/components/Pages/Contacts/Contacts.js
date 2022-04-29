48b1c92819355659f6440169679cda9fe49cc0fc
Alex <Bavalex@outlook.com>
2022-04-29 15:03:10 +0300

    Закладки в контактах

--- src/components/Pages/Contacts/Contacts.js
+++ src/components/Pages/Contacts/Contacts.js
@@ -4,7 +4,8 @@ import Titleh2 from "../../Blocks/Title_H2/Titleh2";
 import Accent from "../../Blocks/accent/Accent";
 import Separator from "../../Blocks/Separator/Separator";
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
+// import Tabs  from "react-bootstrap/Tabs"
+import {Tabs, Tab} from 'react-bootstrap';
 const Contacts = (props) => {
     document.title = props.title
     document.querySelector('meta[name="description"]').content = props.description
@@ -17,26 +18,34 @@ const Contacts = (props) => {
                 transitionEnter={false}
                 transitionLeave={false}>
             {/*<hr/>*/}
-            <Titleh1 name={'Наши контакты'} subname={'самый удобный способ Telegram'}/>
-                <Separator />
-            <div className="container">
-                <div className="row flex-center">
-                    <div className="col-12 col-md-6">
-                        <img src="/images/contactspng.png" width={'100%'} alt=""/>
+            <Tabs defaultActiveKey="Контакты" id="uncontrolled-tab" className="mb-3">
+                <Tab eventKey="Контакты" title="Контакты">
+                    <Titleh1 name={'Наши контакты'} subname={'самый удобный способ Telegram'}/>
+                    <Separator />
+                    <div className="container">
+                        <div className="row flex-center">
+                            <div className="col-12 col-md-6">
+                                <img src="/images/contactspng.png" width={'100%'} alt=""/>
+                            </div>
+                            <div className="col-12 col-md-6">
+                                <b className={'Lage_text'}>
+                                    Наш телефон: +7 960 311 21 05 <br/>
+                                    WhatsApp и <a href={'https://t.me/dicom_team'}>Telegram</a> <br/>
+                                    e-mail: bavalex24@yandex.ru
+                                </b>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-12 col-md-6">
-                        <b className={'Lage_text'}>
-                            Наш телефон: +7 960 311 21 05 <br/>
-                            WhatsApp и <a href={'https://t.me/dicom_team'}>Telegram</a> <br/>
-                            e-mail: bavalex24@yandex.ru
-                        </b>
-                    </div>
-                </div>
-            </div>
 
-            <Separator />
-            <Titleh2 name={'Наш офис'} subname={'Чебоксары, пр. Ленина 12Б'} />
-            <Accent context={'Коворкинг центр имеет пропускную систему, поэтому предварительно позвоните мне или любым другим способом сообщите о предстоящей встрече.'}/>
+                </Tab>
+                <Tab eventKey="Офис" title="Офис">
+                    <Titleh2 name={'Наш офис'} subname={'Чебоксары, пр. Ленина 12Б'} />
+                    <Accent context={'Коворкинг центр имеет пропускную систему, поэтому предварительно позвоните мне или любым другим способом сообщите о предстоящей встрече.'}/>
+                </Tab>
+                <Tab eventKey="Реквизиты" title="Реквизиты">
+                    Реквизиты
+                </Tab>
+            </Tabs>
             </ReactCSSTransitionGroup>
         </>
     );
