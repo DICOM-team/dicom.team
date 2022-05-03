c22976bed9b503700d286c712de16276a9515a4d
Alex <Bavalex@outlook.com>
2022-05-03 11:12:15 +0300

    Рекивзиты ИП добавлены

--- src/State/State.js
+++ src/State/State.js
@@ -1,15 +1,15 @@
 export let mainMenu = {
     items: [
         {name: 'Услуги', urm: '/uslugi', img: '/images/dicomYslugi.png'},
-        {name: 'Сервисы', urm: '/services', img: '/images/icon1.png'},
         {name: 'Кейсы', urm: '/cases', img: '/images/dicomCase.png'},
         {name: 'Команда', urm: '/team', img: '/images/dicomCommand.png'},
         {name: 'Контакты, реквизиты', urm: '/contacts', img: '/images/dicomContacts.png'},
-        {name: 'Support', urm: '/promotion', img: '/images/dicomHelp.png'},
+        {name: 'Вакансии', urm: '/vacancys', img: '/images/dicomVakan.png'},
+        // {name: 'Support', urm: '/promotion', img: '/images/dicomHelp.png'},
         {name: 'Битрикс24', urm: '/bitrix24', img: '/images/dicomB24.png'},
         {name: '1C', urm: '/1c', img: '/images/dicom1c.png'},
-        {name: 'Вакансии', urm: '/vacancys', img: '/images/dicomVakan.png'},
-        {name: 'Telegram bots', urm: '/telegram-bot', img: '/images/telegram.png'}
+        {name: 'Telegram bots', urm: '/telegram-bot', img: '/images/telegram.png'},
+        {name: 'Сервисы', urm: '/services', img: '/images/icon1.png'}
     ]
 }
 export let Form = {
--- src/components/Pages/Contacts/Contacts.js
+++ src/components/Pages/Contacts/Contacts.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Titleh1 from "../../Blocks/Title_H1/Titleh1";
 import Titleh2 from "../../Blocks/Title_H2/Titleh2";
+import Titleh3 from "../../Blocks/Title_H3/Titleh3";
 import Accent from "../../Blocks/accent/Accent";
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import {Tabs, Tab} from 'react-bootstrap';
+
+
 const Contacts = (props) => {
     document.title = props.title
     document.querySelector('meta[name="description"]').content = props.description
@@ -19,7 +21,7 @@ const Contacts = (props) => {
             {/*<hr/>*/}
             <Tabs defaultActiveKey="Контакты" id="uncontrolled-tab" className="mb-3">
                 <Tab eventKey="Контакты" title="Контакты">
-                    <Titleh1 name={'Наши контакты'} subname={'самый удобный способ Telegram'}/>
+                    <Titleh2 name={'Наши контакты'} subname={'самый удобный способ Telegram'}/>
                     {/*<Separator />*/}
                     <div className="container">
                         <div className="row flex-center">
@@ -42,7 +44,95 @@ const Contacts = (props) => {
                     <Accent context={'Коворкинг центр имеет пропускную систему, поэтому предварительно позвоните мне или любым другим способом сообщите о предстоящей встрече.'}/>
                 </Tab>
                 <Tab eventKey="Реквизиты" title="Реквизиты">
-                    Реквизиты
+                    <Titleh3 name={'Реквизиты'} subname={'ИП Бакшандаев А.В.'} />
+                    <p>
+                        <table className={'table table-striped'}>
+                            <tbody>
+                            <tr>
+                                <td valign="middle">
+                                    Полное наименование
+                                </td>
+                                <td valign="middle">
+                                   Индивидуальный предприниматель <br/> Бакшандаев Алексей Валерьевич
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    Сокращенное наименование
+                                </td>
+                                <td valign="middle">
+                                    ИП Бакшандаев А.В.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    ИНН
+                                </td>
+                                <td valign="middle">
+                                    212906995375
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    ОГРНИП
+                                </td>
+                                <td valign="middle">
+                                    321213000010599
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    E-mail
+                                </td>
+                                <td valign="middle">
+                                    <a href="mailto:bavalex24@yandex.ru">bavalex24@yandex.ru</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <Titleh3 name={'Реквизиты банка'} subname={'Сбербанк'} />
+                        <table className={'table table-striped'}>
+                            <tbody>
+                            <tr>
+                                <td valign="middle">
+                                    Расчетный счет
+                                </td>
+                                <td valign="middle">
+                                    40802810775000001204
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    Наименование банка
+                                </td>
+                                <td valign="middle">
+                                    ЧУВАШСКОЕ ОТДЕЛЕНИЕ N8613 ПАО СБЕРБАНК
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    Корреспондентский счет
+                                </td>
+                                <td valign="middle">
+                                    30101810300000000609
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="middle">
+                                    БИК
+                                </td>
+                                <td valign="middle">
+                                    049706609
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <Accent context={
+                            <>
+                                ВНИМАНИЕ! Оплата счета по указанным реквизатам означает согласие с условиями договора-оферта, размещенного по адресу в сети интернет: http://bavalex.bitrix24.site/dogovoroferta/
+                            </>
+                        }/>
+                    </p>
                 </Tab>
             </Tabs>
             </ReactCSSTransitionGroup>
