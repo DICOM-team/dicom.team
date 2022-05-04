adeb7a98f4d28ad8a739d6b3b582f84c0e03e01e
Alex <Bavalex@outlook.com>
2022-05-04 14:47:07 +0300

    Новые списки на странице TelegramBot.js

--- src/components/Pages/TelegramBot/TelegramBot.js
+++ src/components/Pages/TelegramBot/TelegramBot.js
@@ -7,12 +7,59 @@ import TextImg from "../../Blocks/TextImg/TextImg";
 import MyForm from "../../Form/Form";
 import Popup from "../../PopUp/Popup";
 import WhiteBlock from "../../Blocks/WhiteBlock/WhiteBlock";
-import Separator_free from "../../Blocks/Separator_free/Separator_free";
+import SeparatorFree from "../../Blocks/Separator_free/Separator_free";
+import s from './TelegramBot.module.css'
 
 const TelegramBot = (props) => {
     document.title = props.title
     document.querySelector('meta[name="description"]').content = props.description
     document.documentElement.scrollIntoView(true);
+
+    let Lists = {
+        preimushestva: [
+            {id: 1, title: 'Доступ с любых устройств.', text: 'Вы экономите, так как нет необходимости разрабатывать отдельные приложения для Android, IPhone и компьютера - диалоги автоматически синхронизируются со всеми устройствами.'},
+            {id: 2, title: 'Мгновенные ответы.', text: 'Клиенты быстро получают ответы на вопросы в сообщениях от бота, это снижает время ответа на заявку, в результате конверсия увеличивается. Вы легко можете изменить ответы клиентам.'},
+            {id: 3, title: 'Каналы и чаты.', text: 'Боты работают как независимо, так и внутри чатов, и позволяют получить мгновенный ответ, например, для уточнения цен на услуги и наличия товаров.'},
+            {id: 4, title: 'Конфиденциальность.', text: 'Никто не получит доступ к вашей переписке с покупателями.'}
+        ],
+        takze: [
+            {id: 1, title: 'Интеграция.', text: 'Боты без проблем интегрируются с бизнес-приложениями, такими так 1С, CRM, CMS.'},
+            {id: 2, title: 'Быстрый доступ к контактам.', text: 'Боты позволяют быстро получить от клиентов контактные данные, такие как телефон и местонахождение, для передачи их менеджерам. Воронка продаж сокращается!'},
+            {id: 3, title: 'Без регистрации.', text: 'Авторизация не требуется - достаточно установки клиента Telegram, которым сейчас пользуются более 500 000 000 человек.'},
+            {id: 4, title: 'Прием оплаты.', text: 'Благодаря интеграции с платежными системами бот может принимать безопасные платежи от клиентов.'},
+            {id: 5, title: 'Статистика.', text: 'У вас будет полная статистика обращений и переписок с клиентами.'}
+        ]
+    }
+    let preimushestva = Lists.preimushestva.map ( (item) => {
+        return (
+            <div key={item.id} className={'row p-4'}>
+                <div className={s.listNumber + ' col-1 col-lg-1 align-self-center'}>
+                    {item.id}
+                </div>
+                <div className={'col-11 col-lg-3 text-start align-self-center'}>
+                    <b>{item.title}</b>
+                </div>
+                <div className={'col-12 col-lg-8 text-start align-self-center'}>
+                    {item.text}
+                </div>
+            </div>
+        )
+    })
+    let takze = Lists.takze.map ( (item) => {
+        return (
+            <div key={item.id} className={'row p-4'}>
+                <div className={s.listNumber + ' col-1 col-lg-1 align-self-center'}>
+                    {item.id}
+                </div>
+                <div className={'col-11 col-lg-3 text-start align-self-center'}>
+                    <b>{item.title}</b>
+                </div>
+                <div className={'col-12 col-lg-8 text-start align-self-center'}>
+                    {item.text}
+                </div>
+            </div>
+        )
+    })
     return (
         <>
             <ReactCSSTransitionGroup
@@ -23,14 +70,14 @@ const TelegramBot = (props) => {
                 transitionLeave={false}>
                 <Titleh1 name={'Разработка приложений (ботов) для Telegram'} subname={'любой сложности'} />
 
-                <Separator_free />
+                <SeparatorFree />
 
                 <ImgText url={'/images/bot-telegram.png'} text={<>
                     <p>Телеграм-боты — это новый, быстро набирающий популярность инструмент для бизнеса, который позволяет компаниям упростить и снизить стоимость поддержки клиентов.</p>
                     <p>Стоимость разработки ботов в разы дешевле создания мобильного приложения, их не нужно устанавливать. По статистике, сегодня Telegram — это самый популярный мессенджер, проще говоря, почти вся ваша целевая аудитория им уже пользуются.</p>
                 </>} title={'Разработка от 2 недель'} subname={'включает составлние ТЗ'}/>
 
-                <Separator_free />
+                <SeparatorFree />
 
                 <WhiteBlock content={
                     <TextImg url={'/images/telega_phone.png'} text={<>
@@ -39,52 +86,20 @@ const TelegramBot = (props) => {
                     </>} Title={'Любые бизнес-задачи'} subname={''} />
                 } />
 
-                <Separator_free />
+                <SeparatorFree />
 
                 <Titleh2 name={'Преимущества Telegram-ботов'} subname={''} />
                 <p>
-                    <b> Доступ с любых устройств.</b>
-                    <p>
-                    Вы экономите, так как нет необходимости разрабатывать отдельные приложения для Android, IPhone и компьютера - диалоги автоматически синхронизируются со всеми устройствами.
-                    </p>
-                    <b> Мгновенные ответы.</b>
-                    <p>
-                    Клиенты быстро получают ответы на вопросы в сообщениях от бота, это снижает время ответа на заявку, в результате конверсия увеличивается. Вы легко можете изменить ответы клиентам.
-                    </p>
-                    <b>Каналы и чаты.</b>
-                    <p>
-                    Боты работают как независимо, так и внутри чатов, и позволяют получить мгновенный ответ, например, для уточнения цен на услуги и наличия товаров.
-                    </p>
-                    <b>Конфиденциальность.</b>
-                    <p>
-                        Никто не получит доступ к вашей переписке с покупателями.
-                    </p>
+                    {preimushestva}
                 </p>
-                <Separator_free />
-                <Titleh2 name={'А еще телеграм-боты могут'} subname={''} />
-                <Separator_free />
-                <p>
-                    <b>Интеграция.</b>
+                <SeparatorFree />
+                <WhiteBlock content={ <>
+                    <Titleh2 name={'А еще телеграм-боты могут'} subname={''} />
                     <p>
-                    Боты без проблем интегрируются с бизнес-приложениями, такими так 1С, CRM, CMS.
+                        {takze}
                     </p>
-                    <b>Быстрый доступ к контактам.</b>
-                    <p>
-                    Боты позволяют быстро получить от клиентов контактные данные, такие как телефон и местонахождение, для передачи их менеджерам. Воронка продаж сокращается!
-                    </p>
-                    <b>Без регистрации.</b>
-                    <p>
-                    Авторизация не требуется - достаточно установки клиента Telegram, которым сейчас пользуются более 500 000 000 человек.
-                    </p>
-                    <b>Прием оплаты.</b>
-                    <p>
-                    Благодаря интеграции с платежными системами бот может принимать безопасные платежи от клиентов.
-                    </p>
-                    <b>Статистика.</b>
-                    <p>
-                    У вас будет полная статистика обращений и переписок с клиентами.
-                    </p>
-                </p>
+                </>} />
+                <SeparatorFree />
                 <Popup button={'Консультация перед заказом'} title={'Бесплатная часовая консультация'} content={<><MyForm /></>}/>
             </ReactCSSTransitionGroup>
         </>
