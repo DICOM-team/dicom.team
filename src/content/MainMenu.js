6bb63a54643ad09c4dd5ce5d2e6f6bbf4a750868
Alex <Bavalex@outlook.com>
2022-10-24 14:00:09 +0300

    Игра "Ну погоди"

--- src/content/MainMenu.js
+++ src/content/MainMenu.js
@@ -12,6 +12,6 @@ export let mainMenu = {
         {name: 'Сервисы', urm: '/services', img: '/images/icons/icon1.png'},
         //{name: 'Aspro', urm: '/aspro', img: '/images/icons/aspro.png'},
         {name: 'Сайты', urm: '/Sites', img: '/images/icons/Sites.png'},
-        // {name: 'Игры', urm: '/games', img: '/images/icons/Tilda_logo.png'}
+        {name: 'Игры', urm: '/games', img: '/images/icons/Tilda_logo.png'}
     ]
 }
